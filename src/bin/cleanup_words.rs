e97a68b7f40eb1fdcc1b59a6745b415fc3870446
Mieszko Sabo <mieszko.sabo@icloud.com>
2022-09-09 17:22:19 +0200

    all popular polish words

---
+++ src/bin/cleanup_words.rs
@@ -0,0 +1,14 @@
+use std::fs::{read_to_string, write};
+use unicode_segmentation::{self, UnicodeSegmentation};
+
+fn main() {
+    let words = read_to_string("assets/game_words.txt")
+        .expect("could not open game_words.txt")
+        .lines()
+        .filter(|w| w.graphemes(true).count() >= 3)
+        .filter(|w| !w.contains(" "))
+        .collect::<Vec<&str>>()
+        .join("\n");
+
+    write("assets/game_words.txt", words).expect("could not write to game_words.txt");
+}
